62954bf49861f806807d52c8f1dbc09767a072a0
Ziyoda <sharipovaziyoda@example.com>
2024-11-09 00:06:25 +0500

    Files added

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,26 @@ import Register from "./components/Register";
 import VerifyOtp from "./components/VerifyOtp";
 import Sign from "./components/Sign";
 import UserProfileToday from "./components/UserProfileToday";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <>
+   
+    <Navbar username="John Doe" email="johndoe@example.com" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="register" element={<Register />} />
         <Route path="verify-otp" element={<VerifyOtp />} />
         <Route path="sign" element={<Sign />} />
         <Route path="UserProfileToday" element={<UserProfileToday />} />
+        <Route path="/daily" element={<div/>} />
+         <Route path="/weekly" element={<div/>} />
+         <Route path="/monthly" element={<div/>} />
+         <Route path="/add-special-day" element={<div/>} />
       </Routes>
+      
     </>
   );
 }
