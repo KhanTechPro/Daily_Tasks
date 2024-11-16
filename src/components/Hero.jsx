fd70f95d417f544c97607810b2ef7ced36cb07b4
Qosimova Muminaxon <07janeokey07@gmail.com>
2024-11-16 22:40:04 +0500

    fixing direct sign-in

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
     <>
+      <Navbar />
       <div className="max-w-[1024px] mx-auto p-4 flex flex-col justify-center items-center text-center relative md:top-[120px] top-[100px]">
         <h1 className="md:text-6xl text-4xl font-medium py-4">Daily Tasks</h1>
         <p className="md:text-xl text-md">
