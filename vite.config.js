cb902d187b296108ffd90800b3b3102dbd8547a9
Qosimova Muminaxon <07janeokey07@gmail.com>
2024-11-23 14:43:29 +0500

    connecting API

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      external: ["axios"], // Exclude axios from the bundle
+      external: ["/axios"], // Exclude axios from the bundle
     },
   },
 });
