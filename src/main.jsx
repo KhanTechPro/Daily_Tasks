a044d60a21b3c8cde150bc9189c6795257aabec2
Qosimova Muminaxon <07janeokey07@gmail.com>
2024-11-24 14:45:19 +0500

    connecting API

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // Ensure the correct path
+import "../index.css"; // Ensure the correct path
 import { UserProvider } from "./components/UserContext";
 
 const rootElement = document.getElementById("root");
